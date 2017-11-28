851fce35a781deb9c22e58da83e440a4cdacc624
Carl Stermann-Lücke <carl.stermann-luecke@st.ovgu.de>
2017-11-28 20:57:27 +0100

    Fixed some syntax errors in setup_page.js. Moved the script import to child html files.

--- app/static/js/setup_page.js
+++ app/static/js/setup_page.js
@@ -1,24 +1,23 @@
 
 var setup = new Vue({
-    console.log("Test log");
     el: '#setup',
     data: {
-        options: [];
-        criteria: [];
-        question: "";
+        alternatives: [],
+        criteria: [],
+        question: ""
     },
     delimiters: ["[[", "]]"],
     methods: {
-        addOption: ()=> {
-            options.push('');
+        addAlternative: ()=> {
+            alternatives.push('');
         },
         addCriterion: ()=> {
             criteria.push({name: '', description: '', weight: 1});
         },
-        removeOption: (id)=> {
-            var index = this.options.indexOf(id);
-            options.splice(index, 1);
-        }
+        removeAlternative: (id)=> {
+            var index = this.alternatives.indexOf(id);
+            alternatives.splice(index, 1);
+        },
         removeCriterion: (id)=> {
             var index = this.criteria.indexOf(id);
             criteria.splice(index, 1);
