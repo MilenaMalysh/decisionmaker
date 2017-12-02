434d3b93d777048abef5f07c6aa17a16ef9419f5
Carl Stermann-Lücke <carl.stermann-luecke@st.ovgu.de>
2017-12-02 14:56:49 +0100

    Added submit-button. Error: Submit function is not defined.

--- app/static/js/setup_page.js
+++ app/static/js/setup_page.js
@@ -8,11 +8,39 @@ var setup = new Vue({
     },
     delimiters: ["[[", "]]"],
     methods: {
+        validate(){
+            console.log('aaaa');
+            titles = _.every(alternatives.title, function (title) {
+                            return title !== '';
+                        });
+            names = _.every(criteria.name, function (name) {
+                            return name !== '';
+                        });
+            descriptions = _.every(criteria.description, function (description) {
+                            return description !== '';
+                        });
+            weights = _.every(criteria.weight, function (weight) {
+                            return weight !== 0;
+                        });
+            questionDefined = function(){
+                this.question !== '';
+            };
+            return titles && names && descriptions && weights && questionDefined;
+        },
+        submit(){
+            alert('test alert');
+            if(!validate()){
+                alert('Please fill in all the fields or delete unwanted alternatives and criteria.');
+            } else{
+                //submit data
+            }
+        },
         addAlternative(){
-            this.alternatives.push({});
+            this.alternatives.push({title: ''});
+            submit();
         },
         addCriterion(){
-            this.criteria.push({name: '', description: '', weight: 1});
+            this.criteria.push({name: '', description: '', weight: 0});
         },
         removeAlternative(index){
             this.alternatives.splice(index, 1);
