f9414beb2b00fed1aceff6afe82cb300818df0a5
Ivan Prymak <milenamalush1@gmail.com>
2018-01-15 16:30:33 +0100

    remove keyring in favour of env secret

--- app/emails.py
+++ app/emails.py
@@ -1,3 +1,4 @@
+import os
 import yagmail
 from django.urls import reverse
 
@@ -5,6 +6,7 @@ from django.urls import reverse
 def send_invitation(request, invitation):
     decision = invitation.decision.name
     url = request.build_absolute_uri(reverse('filling', args=[invitation.id]))
-    yagmail.SMTP('zenclosure').send(invitation.user.email, "Decisionmaker:" + decision,
+    password = os.getenv('EMAIL_PASSWORD')
+    yagmail.SMTP('zenclosure', password).send(invitation.user.email, "Decisionmaker:" + decision,
                                     "There are decision worth making! <br> You were invited to "
                                     "<a href=\"{url}\">{url}</a> to make a decision.".format(url=url))
