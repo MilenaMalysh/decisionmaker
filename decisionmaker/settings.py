5447d6b8b757667db2b633deaa4c49f90f27b4b7
Ivan <esquilo1994@gmail.com>
2018-01-17 01:46:19 +0100

    remove static files dir

--- decisionmaker/settings.py
+++ decisionmaker/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 #STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.FileSystemFinder',
