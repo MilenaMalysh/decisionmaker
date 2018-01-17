6f14ae0f25b9344d9e7aaecac03a26fd8a45c703
Ivan <esquilo1994@gmail.com>
2018-01-17 01:42:20 +0100

    remove static files finder

--- decisionmaker/settings.py
+++ decisionmaker/settings.py
@@ -132,10 +132,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+#STATICFILES_FINDERS = (
+#    'django.contrib.staticfiles.finders.FileSystemFinder',
+#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#)
 
 django_heroku.settings(locals())
 
